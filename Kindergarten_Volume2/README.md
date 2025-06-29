c7bc6006d80a73dceee5dbc2be95e38fb90c126a
Sungbae Jeong <almagest0328@gmail.com>
2025-06-29 21:50:25 +0900

    update example

--- Kindergarten_Volume2/README.md
+++
@@ -1,4 +0,0 @@
-# example code for vesti
-
-Kindergarten is the book written by myself. Personally, this is a great example
-for vesti
--- Kindergarten_Volume2/build.lua
+++ Kindergarten_Volume2/build.lua
@@ -1,3 +1,15 @@
-os.execute("bibtex ./.vesti-dummy/kindergarten-vol2.aux")
-os.execute("makeindex -s ./kindergarten.ist ./.vesti-dummy/kindergarten-vol2.idx")
-os.execute("makeindex ./.vesti-dummy/sym.idx")
+local err = vesti.setCurrentDir("./.vesti-dummy")
+if err ~= nil then
+	vesti.printError(err)
+	os.exit(1)
+end
+
+os.execute("bibtex ./kindergarten-vol2.aux")
+os.execute("makeindex -s ../kindergarten.ist ./kindergarten-vol2.idx")
+os.execute("makeindex ./sym.idx")
+
+local err = vesti.setCurrentDir("..")
+if err ~= nil then
+	vesti.printError(err)
+	os.exit(1)
+end
