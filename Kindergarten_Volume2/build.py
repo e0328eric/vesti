94aa1463b5c1c75d5b7ca9092dca1b6fc218b8fa
Sungbae Jeong <almagest0328@gmail.com>
2025-08-21 01:09:23 +0900

    build.lua -> build.py

---
+++ Kindergarten_Volume2/build.py
@@ -0,0 +1,10 @@
+import os
+import subprocess as sp
+
+print("Run bibtex and makeindex")
+
+os.chdir("./.vesti-dummy")
+sp.run(["bibtex", "./kindergarten-vol2.aux"])
+sp.run(["makeindex", "-s", "../kindergarten.ist", "./kindergarten-vol2.idx"])
+sp.run(["makeindex", "./sym.idx"])
+os.chdir("..")
