762899f5dc17927e379122d9b200bbd2fac8c84e
Sungbae Jeong <almagest0328@gmail.com>
2025-12-04 16:13:32 +0900

    remove aapproach example

--- AApproach/first.lua
+++
@@ -1,8 +0,0 @@
-local f <close> = io.open("font.ves")
-if f == nil then
-	vesti.getModule("template")
-end
-
-vesti.compile("aapproach.ves", {
-	compile_all = true,
-})
--- AApproach/step.lua
+++
@@ -1,6 +0,0 @@
-if vesti.engineType() ~= "tect" then
-    local cwd = vesti.getCurrentDir()
-    vesti.setCurrentDir(vesti.vestiDummyDir())
-    os.execute("bibtex aapproach")
-    vesti.setCurrentDir(cwd)
-end
